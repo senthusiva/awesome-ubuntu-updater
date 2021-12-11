6903d2cbbc6f84c7150af031553b944f5d95defb
Senthu Siva <18628402+senthusiva@users.noreply.github.com>
2021-12-11 14:06:53 +0100

    new script to get github user info

---
+++ src/githubinfo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <username>"
+  exit 1
+fi
+
+username=$1
+function getInfo() {
+    curl -s "https://api.github.com/users/$username" | jq -r "$1"
+}
+
+echo -e "Name: " $(getInfo ".name")
+echo -e "URL: " $(getInfo ".html_url")
+echo -e "Company: " $(getInfo ".company")
+echo -e "Website: " $(getInfo ".blog")
+echo -e "E-Mail: " $(getInfo ".email")
+echo -e "Followers: " $(getInfo ".followers")
+echo -e "Following: " $(getInfo ".following")
+echo -e "Created at: " $(getInfo ".created_at")
+echo -e "Last updated: " $(getInfo ".updated_at")
+
+
+
+
+
+
+
